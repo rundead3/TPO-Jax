34f9317bd99f1388979e5f4013f8f2896e8cf2e1
Rodrigo Bernardino <74833832+rundead3@users.noreply.github.com>
2025-01-25 17:10:33 +0000

    Update residue_constants.py

--- unifold/common/residue_constants.py
+++ unifold/common/residue_constants.py
@@ -509,10 +509,10 @@ atom_types = [
     'N', 'CA', 'C', 'CB', 'O', 'CG', 'CG1', 'CG2', 'OG', 'OG1', 'SG', 'CD',
     'CD1', 'CD2', 'ND1', 'ND2', 'OD1', 'OD2', 'SD', 'CE', 'CE1', 'CE2', 'CE3',
     'NE', 'NE1', 'NE2', 'OE1', 'OE2', 'CH2', 'NH1', 'NH2', 'OH', 'CZ', 'CZ2',
-    'CZ3', 'NZ', 'OXT'
+    'CZ3', 'NZ', 'OXT', 'P', 'O1P', 'O2P', 'O3P'  # Added phosphate atoms
 ]
 atom_order = {atom_type: i for i, atom_type in enumerate(atom_types)}
-atom_type_num = len(atom_types)  # := 37.
+atom_type_num = len(atom_types)  # := 41
 
 # A compact atom encoding with 14 columns
 # pylint: disable=line-too-long
